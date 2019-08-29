5c1a0e31bd2235c303d0e4c704dda8d288b004f2
Victor Chugunov <vichugunov@gmail.com>
2019-08-29 07:32:35 +0300

    cleanup

--- test/TestController.test.ts
+++ test/TestController.test.ts
@@ -7,18 +7,16 @@ import { expect } from 'chai'
 describe('TestController', () => {
   let request: SuperTest.SuperTest<SuperTest.Test>
   before(bootstrap(server))
-  let routeService: RouteService
   let expressApplication: ExpressApplication
 
   before(inject([ExpressApplication, RouteService], (...args) => {
-    [expressApplication, routeService] = args
+    [expressApplication] = args
     request = SuperTest(expressApplication)
   }))
 
   after(TestContext.reset)
 
   it('should be forbidden', async () => {
-    console.log('routes', routeService.getRoutes())
     const response = await request.get('/test/hello').expect(200)
     return expect(response.text).to.eq('world')
   })
